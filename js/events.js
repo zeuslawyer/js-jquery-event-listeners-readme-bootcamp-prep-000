454595966b2aabadc32f3d1269fe41d5bf5b6eee
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-06-12 06:41:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(e){
+    alert('Hey!');
+  });
+}
+
+
+function frameIt(){
+  $('.tasty').on('load', function(e){
+    
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if(e.which == 71) {
+      alert('you hit "G"!');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',  function(e){
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
 
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
 });
+
+
+
